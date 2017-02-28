4698f852e1a54dc9a1025b8fbadde26974e8f026
Soel <shachar.soel@sap.com>
2017-02-28 20:29:49 +0200

    syntax-cli performance WIP

---
+++ performance/jsonParsers/syntax/gen_and_bundle.sh
@@ -0,0 +1,2 @@
+../../../node_modules/syntax-cli/bin/syntax --grammar syntax_json.grammar.js --mode SLR1 --output syntax_json_parser.js
+../../../node_modules/.bin/browserify ./syntax_json_parser.js -o ./syntax_json_parser_browser.js -s syntax_json_parser
---
+++ performance/jsonParsers/syntax/readme.md
@@ -0,0 +1,2 @@
+* To generate: ../../../node_modules/syntax-cli/bin/syntax --grammar syntax_json.grammar.js --mode SLR1 --output syntax_json_parser.js
+* to create browser module: ../../../.bin/browserify  
